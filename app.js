15d6e516fe40ff25dbed9c56a83dac5d91d43afc
Josh Divak <joshdivak2023@u.northwestern.edu>
2021-04-08 00:25:06 -0500

    Fixed first outcome

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //This is a story-based game for exploring the criminal justice system and its impact.
 
 //tracks progress
-let stage = 0;
+let stage = -1;
 let outcomes = 0;
 
 //content for each stage
@@ -11,8 +11,8 @@ const stageNames = [
     "Story", 
     "Police", 
     "Will excessive force be used?", 
+    "No excessive force",
     "Excessive force used", 
-    "No excessive force", 
     "Bail?", 
     "$2000 bail", 
     "Can you pay bail?", 
@@ -26,8 +26,8 @@ const story = [
     "When he was 22 years old, Jerome and a couple other guys were struggling financially. One of his friends had the idea to break into a house in a nicer neighborhoods.",
     "The house had a security system that automatically called the police...",
     "The police often use excessive force. Will the police use excessive force on Jerome?",
-    "The police used excessive force and Jerome was injured during his arrest",
     "The police arrested Jerome without the use of force",
+    "The police used excessive force and Jerome was injured during his arrest",
     "Some defendents are released without financial bail - see if Jerome got lucky.",
     "Unfortunately, Jerome was held on $2000 bail.",
     "Will Jerome be able to pay his bail?",
@@ -70,10 +70,13 @@ function replaceImage(){
 function outcomeMessage(occur){
     if (outcomes == 0) {
         if (occur == true) {
+            // removes later possibilities from the arrays. In this instance, it removes the part where "Excessive force was used" shows up
+            stageNames.splice(stage+2,1);
+            story.splice(stage+2,1);
             return "Excessive force was not used";
-            stage = stage+1;
         }
         else {
+            stage = stage+1;
             return "Excessive force was used";
         } 
     }
@@ -102,8 +105,10 @@ function probMachine(){
 function changeColor(){
     //probability of desired outcome
     prob = probability[stage];
+    console.log(prob);
     //random outcome
     rand = 20+Math.round(Math.random()*30);
+    console.log(rand);
     first = document.getElementById("icon0");
     first.style.color = "yellowgreen";
     let i = 1;
@@ -124,15 +129,13 @@ function changeColor(){
                 clearInterval(int);
                 let outcome = (rand % 10);
                 if (outcome > prob){
-                    occur = true
+                    occur = true;
                     let message = outcomeMessage(occur);
                     window.alert(message);
-                    nextStage(5);
                 } else{
                     occur = false;
                     let message = outcomeMessage(occur);
                     window.alert(message);
-                    nextStage(6);
                 }
             }
     }, 100);  
